6f07765f660fba245ba1950cba8d0f7186a51489
Couasnon <anais.couasnon@vu.nl>
2020-08-21 13:22:10 +0200

    Added rows where Max can improve

--- .ipynb_checkpoints/Build_Ruebisbaach_extreme_rainfall-checkpoint.py
+++ .ipynb_checkpoints/Build_Ruebisbaach_extreme_rainfall-checkpoint.py
@@ -1,17 +1,18 @@
+# -*- coding: utf-8 -*-
 '''
-Adapted from: Andres Diaz and Philip Ward
-Coded into Python by Eric Mortensen and Anaïs Couasnon
-Date: 2020
+# Adapted from: Andres Diaz and Philip Ward
+# Coded into Python by Eric Mortensen and Anaïs Couasnon
+# Date: 2020
 
-DESCRIPTION:
-This script prepares the precipitation data, aggregating the values in
-different rainfall durations.
+# DESCRIPTION:
+# This script prepares the precipitation data, aggregating the values in
+# different rainfall durations.
 
-The homework consists of constructing empirical and analytical IDF curves from the 
-Ruebisbaach precipitation data.
+# The homework consists of constructing empirical and analytical IDF curves from the 
+# Ruebisbaach precipitation data.
+#%% importing relevant packages
 '''
-
-#%% 
+# %% Importing the relevant packages
 import os, sys
 import pandas as pd
 import datetime
@@ -22,8 +23,7 @@ from scipy.interpolate import Rbf, InterpolatedUnivariateSpline, UnivariateSplin
 
 from functions_Ruebisbaach import height_to_intensity, Gumbelfit_EM, Gumbel_evfit, empirical_T
 
-#%% 
-# Define folder paths and filenames
+# %% Define folder paths and filenames
 #Folder where your data and scripts are
 root_folder = r'E:\surfdrive\Shared\Water_Risks\2019_2020\SESSION_MATERIALS\SESSION_3_Flood_Hazard_I\Practical_Session\UPLOAD' 
 raw_rainfall_data = 'Ruebisbaach_precipitation_RAW_AC.csv'
@@ -55,44 +55,36 @@ matrixPrec1hr.rename(columns={'0.5hr': '1hr'}, inplace = True)
 
 # We extract the extreme values using the Annual Maxima method
 extreme_raw = matrixPrec.resample('AS').max() #Annual maxima of 30-min rainfall
-#date_max_raw =  #MAX: GETTING THE INDEX AT WHICH MAXIMUM OCCURED #####################################
-
-# This is always recommend to first have a look at the data before using it!
-plt.figure()
-plt.plot(matrixPrec.index, matrixPrec.loc[:,'0.5hr'], '.-b')
-#ADD a line to plot point
-plt.ylabel('Rainfall (mm)')
-plt.xlabel('Date')
-plt.show()
+#date_max_raw = #####################################################################
 
 extreme1hr = matrixPrec1hr.resample('AS').max() #Annual maxima of 1-hr rainfall
 
 # We combine the results
 output = pd.concat([extreme_raw, extreme1hr], axis = 1)
-# %% To remove before the class -------------------------------------------------------
-# Calculating precipitation for other durations
+#%% To remove before the class -------------------------------------------------------
+# # Calculating precipitation for other durations
 
-duration = [0.5, 1, 3, 6, 12, 24]
+# duration = [0.5, 1, 3, 6, 12, 24]
 
-matrixPrec3hr = matrixPrec.resample('3H').sum()
-matrixPrec3hr.rename(columns={'0.5hr': '3hr'}, inplace = True)
+# matrixPrec3hr = matrixPrec.resample('3H').sum()
+# matrixPrec3hr.rename(columns={'0.5hr': '3hr'}, inplace = True)
 
-matrixPrec6hr = matrixPrec.resample('6H').sum()
-matrixPrec6hr.rename(columns={'0.5hr': '6hr'}, inplace = True)
+# matrixPrec6hr = matrixPrec.resample('6H').sum()
+# matrixPrec6hr.rename(columns={'0.5hr': '6hr'}, inplace = True)
 
-matrixPrec12hr = matrixPrec.resample('12H').sum()
-matrixPrec12hr.rename(columns={'0.5hr': '12hr'}, inplace = True)
+# matrixPrec12hr = matrixPrec.resample('12H').sum()
+# matrixPrec12hr.rename(columns={'0.5hr': '12hr'}, inplace = True)
 
-matrixPrec24hr = matrixPrec.resample('D').sum()
-matrixPrec24hr.rename(columns={'0.5hr': '24hr'}, inplace = True)
+# matrixPrec24hr = matrixPrec.resample('D').sum()
+# matrixPrec24hr.rename(columns={'0.5hr': '24hr'}, inplace = True)
 
-extreme3hr = matrixPrec3hr.resample('AS').max() #Annual maxima of 3-hr rainfall
-extreme6hr = matrixPrec6hr.resample('AS').max() #Annual maxima of 6-hr rainfall
-extreme12hr = matrixPrec12hr.resample('AS').max() #Annual maxima of 12-hr rainfall
-extreme24hr = matrixPrec24hr.resample('AS').max() #Annual maxima of 24-hr rainfall
+# extreme3hr = matrixPrec3hr.resample('AS').max() #Annual maxima of 3-hr rainfall
+# extreme6hr = matrixPrec6hr.resample('AS').max() #Annual maxima of 6-hr rainfall
+# extreme12hr = matrixPrec12hr.resample('AS').max() #Annual maxima of 12-hr rainfall
+# extreme24hr = matrixPrec24hr.resample('AS').max() #Annual maxima of 24-hr rainfall
 
-# We combine the results
-output = pd.concat([extreme_raw, extreme1hr, extreme3hr, extreme6hr, extreme12hr, extreme24hr], axis = 1)
+# # We combine the results
+# output = pd.concat([extreme_raw, extreme1hr, extreme3hr, extreme6hr, extreme12hr, extreme24hr], axis = 1)
 #%% Convert to rainfall intensity
 intensity_mmhr = pd.DataFrame(data = None, columns = output.columns)
 for i in np.arange(0,len(output.columns), 1):
@@ -137,30 +129,30 @@ plt.xlabel('Duration (hrs)')
 plt.ylabel('Rainfall Intensity (mm/hr)')
 
 #%% Make the IDF graph and fit an interpolation
-# Smooth Original IDF Curves using splines: 
-IDF_interp = pd.DataFrame(index = np.linspace(min(duration), max(duration), 100), columns = return_periods)
-for r in IDF.index:
-    tck = UnivariateSpline(duration, IDF.loc[r,:].values, k = 2, s=2)  #MAX : CAN YOU IMPROVE THIS? #############
-    ynew = tck(IDF_interp.index.values)
-    # tck = interpolate.splrep(duration, IDF.loc[r,:].values, s=0)
-    # ynew = interpolate.splev(IDF_interp.index.values, tck, der=0)
-    #tck = Rbf(duration, IDF.loc[r,:].values)
-    #ynew = tck(IDF_interp.index.values)
-    IDF_interp.loc[:,r] = ynew
+# # Smooth Original IDF Curves using splines: 
+# IDF_interp = pd.DataFrame(index = np.linspace(min(duration), max(duration), 100), columns = return_periods)
+# for r in IDF.index:
+#     tck = UnivariateSpline(duration, IDF.loc[r,:].values, k = 2, s=2)
+#     ynew = tck(IDF_interp.index.values)
+#     # tck = interpolate.splrep(duration, IDF.loc[r,:].values, s=0)
+#     # ynew = interpolate.splev(IDF_interp.index.values, tck, der=0)
+#     #tck = Rbf(duration, IDF.loc[r,:].values)
+#     #ynew = tck(IDF_interp.index.values)
+#     IDF_interp.loc[:,r] = ynew
 
-#Plot the results
-plt.figure()
-j = 0
-for dur in IDF.columns:
-    plt.plot(np.repeat(duration[j], len(IDF)), IDF.loc[:,dur].values, '.k')
-    j += 1
+# #Plot the results
+# plt.figure()
+# j = 0
+# for dur in IDF.columns:
+#     plt.plot(np.repeat(duration[j], len(IDF)), IDF.loc[:,dur].values, '.k')
+#     j += 1
 
-for ret_period in IDF_interp.columns:
-    print(ret_period)
-    plt.plot(IDF_interp.index, IDF_interp.loc[:, ret_period], '-')
-plt.xlim(0,30)
-plt.xlabel('Duration (hrs)')
-plt.ylabel('Rainfall Intensity (mm/hr)')
+# for ret_period in IDF_interp.columns:
+#     print(ret_period)
+#     plt.plot(IDF_interp.index, IDF_interp.loc[:, ret_period], '-')
+# plt.xlim(0,30)
+# plt.xlabel('Duration (hrs)')
+# plt.ylabel('Rainfall Intensity (mm/hr)')
 
 #%% METHOD 2 - USING AN EMPIRICAL FUNCTION
 # Fit the empirical function to obtain the IDF. Here we use a power law.
@@ -212,6 +204,22 @@ plt.xlim(0,30)
 plt.xlabel('Duration (hrs)')
 plt.ylabel('Rainfall Intensity (mm/hr)')
 #Add a legend to your figure!
+
+
+
+#%%
+# We save this table to construct our graph
+# Tip: first make a variable to hold the output in that you want (the
+# columns with the data per year for the different durations
+#output = pd.concat(extre3hr(:,2),extre6hr(:,2),extre12hr(:,2),extre24hr(:,2)];
+
+#%%
+# Add lines below to save the output in the same format as in the file
+# "Manzese_rainfall.txt" (but give it a different name!)
+# Then add a line to save this (you can use save: see the Matlab help file
+# to find out how save works...
+
+#save(strcat(root_folder,'\Ruebisbaach_rainfall_IDF.txt'), 'output', '-ascii');
 
 
 
